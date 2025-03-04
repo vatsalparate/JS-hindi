7394f9114b2f63df54cbd5badc66a59bdc767120
vatsalparate <alphawizard21@gmail.com>
2025-03-04 18:09:06 +0000

    03

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+// console.log(typeof score);
+ //console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber); //NaN
+
+
+// "33"=> 33
+/* "33abc' => Nan
+true = 1; false=0*/
+
+let isLoggedIn ="vatsal"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//""=false
+//"vatsal"=true
+
